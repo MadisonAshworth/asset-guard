5d2c2ddc7286fe1e2202e8587dfd7d73e22ea109
madison.ashworth <madison.ashworth@tridentutilities.co.uk>
2026-07-23 12:03:51 +0100

    Created db.ts to create an item schema. and store items locally on RxDB.

---
+++ src/lib/db.ts
@@ -0,0 +1,52 @@
+import { createRxDatabase } from "rxdb";
+import { getRxStorageDexie } from "rxdb/plugins/storage-dexie";
+
+export const itemSchema = {
+  title: "items",
+  version: 0,
+  primaryKey: "id",
+  type: "object",
+  properties: {
+    id: {
+      type: "string",
+      maxLength: 100,
+    },
+    title: {
+      type: "string",
+    },
+    content: {
+      type: "string",
+    },
+    type: {
+      type: "string",
+    },
+    completed: {
+      type: "boolean",
+    },
+    deleted: {
+      type: "boolean",
+    },
+    synced: {
+      type: "boolean",
+    },
+    updatedAt: {
+      type: "number",
+    },
+  },
+  required: ["id", "title"],
+};
+
+export async function createDatabase() {
+  const db = await createRxDatabase({
+    name: "assetguarddb",
+    storage: getRxStorageDexie(),
+  });
+
+  await db.addCollections({
+    items: {
+      schema: itemSchema,
+    },
+  });
+
+  return db;
+}
