fa293403bfa67fa146f26d9e17d0adf16140a379
madison.ashworth <madison.ashworth@tridentutilities.co.uk>
2026-07-23 12:25:41 +0100

    Created useSync.ts as a hook to watch for online connection to the internet. If online the data will be sync by a 5sec interval to update the supabase db. If offline the hook will store the unsynced data to localDb and await for the app to get connectivity, it will then sync unsynced data with supabases online db. Left debug code in to test as I accidently worked on master branch

---
+++ src/hooks/useSync.ts
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import { getItems, markAsSynced } from "@/services/items";
+import { supabase } from "@/lib/supabase";
+
+export function useSync() {
+  console.log("useSync running");
+
+  const [online, setOnline] = useState(
+    typeof window !== "undefined" ? navigator.onLine : true,
+  );
+
+  async function pushToCloud() {
+    try {
+      console.log("pushToCloud started");
+      const items = await getItems();
+      console.table(items);
+      const unsynced = items.filter((item) => !item.synced);
+      console.log("UNSYNCED:", unsynced);
+
+      for (const item of unsynced) {
+        console.log("Uploading:", item);
+
+        const { error } = await supabase.from("items").upsert({
+          id: item.id,
+          title: item.title,
+          content: item.content,
+          type: item.type,
+          completed: item.completed,
+          deleted: item.deleted,
+          updated_at: new Date(item.updatedAt).toISOString(),
+        });
+
+        if (error) {
+          console.error("SUPABASE ERROR:", error);
+        } else {
+          console.log("Upload successful:", item.id);
+
+          await markAsSynced(item.id);
+        }
+      }
+    } catch (err) {
+      console.error("pushToCloud FAILED:", err);
+    }
+  }
+
+  useEffect(() => {
+    console.log("useEffect Started");
+
+    const handleOnline = () => {
+      console.log("FORCED SYNC");
+      setOnline(true);
+      pushToCloud();
+      console.log("Online");
+    };
+
+    const handleOffline = () => {
+      setOnline(false);
+      console.log("Offline");
+    };
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    if (navigator.onLine) {
+      console.log("navigator.onLine = true");
+      pushToCloud();
+    }
+
+    const interval = setInterval(() => {
+      if (navigator.onLine) {
+        pushToCloud();
+      }
+    }, 5000);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+      clearInterval(interval);
+    };
+  }, []);
+
+  return online;
+}
